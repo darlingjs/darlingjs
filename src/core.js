7b61c5d3b33034f22a44566b531c493067088945
hyzhak <hyzhak@ya.ru>
2013-03-19 00:58:25 +0100

    proof of concept

---
+++ src/core.js
@@ -0,0 +1,7 @@
+'use strict';
+/**
+ * Project: GameEngine.
+ * Core of library
+ *
+ * Copyright (c) 2013, Eugene-Krevenets
+ */
---
+++ src/flatWorld.js
@@ -0,0 +1,7 @@
+'use strict';
+/**
+ * Project: GameEngine.
+ * FlatWorld Extensions
+ *
+ * Copyright (c) 2013, Eugene-Krevenets
+ */
