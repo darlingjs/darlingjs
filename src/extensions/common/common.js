ea7a1af6a8d40f312e7f03726c7c1e88836d65a9
Eugene Krevenets <hyzhak@ya.ru>
2013-03-31 16:15:21 +0200

    add common extension with selected component marker

---
+++ src/extensions/common/common.js
@@ -0,0 +1,16 @@
+/**
+ * Project: darlingjs / GameEngine.
+ *
+ * Module with common components and systems:
+ *
+ * * ngSelected - marker for selected entity;
+ *
+ * Copyright (c) 2013, Eugene-Krevenets
+ */
+
+(function(darlingjs) {
+    'use strict';
+
+    var module = darlingjs.module('ngCommon');
+    module.$c('ngSelected');
+})(darlingjs);
