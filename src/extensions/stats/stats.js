8ab858cc23fff7d6586f368bff913e1d9a273e19
Eugene Krevenets <hyzhak@ya.ru>
2013-04-08 10:45:38 +0200

    customize stats placement

--- src/extensions/stats/stats.js
+++ src/extensions/stats/stats.js
@@ -7,15 +7,21 @@
     var m = darlingjs.module('ngStats');
     var stats;
     m.$s('ngStatsBegin', {
+        domId: '',
+        target: null,
+
         $added : function() {
             stats = new Stats();
             stats.setMode( 0 );
-            document.body.appendChild( stats.domElement );
+            this.target = this.target || document.getElementById(this.domId) || document.body;
+            this.target.appendChild( stats.domElement );
         },
+
         $update: function update() {
             stats.begin();
         }
     });
+
     m.$s('ngStatsEnd', {
         $update: function update() {
             stats.end();
