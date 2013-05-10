05df696771c7641dc17c98522ef7f90ea9bb0899
Eugene Krevenets <hyzhak@ya.ru>
2013-05-10 16:14:29 +0200

    add some jsdoc

--- src/extensions/space/cyclic.js
+++ src/extensions/space/cyclic.js
@@ -35,8 +35,21 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfOutsideOfTheViewPortVertical2D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes outside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
@@ -44,8 +57,21 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfOutsideOfTheViewPortHorizontal2D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes outside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
@@ -53,8 +79,21 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfInsideOfTheViewPortVertical2D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes inside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
@@ -62,46 +101,85 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfInsideOfTheViewPortHorizontal2D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes inside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
-    m.$s('ngCyclic2DLayer', {
+    /**
+     * System that implement 2D cyclic layer
+     */
+    m.$s('ng2DCyclicLayer', {
         $require: ['ngCyclic', 'ng2D'],
 
         $addEntity: ['$entity', function($entity) {
             if ($entity.ngCyclic.patternWidth) {
                 insideConfigHorizontal2D.minWidth = $entity.ngCyclic.patternWidth;
-                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal2D', insideConfigHorizontal3D);
+                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal2D',
+                    insideConfigHorizontal2D);
                 outsideConfigHorizontal2D.minWidth = $entity.ngCyclic.patternWidth;
-                $entity.$add('ngMarkIfOutsideOfTheViewPortHorizontal2D', outsideConfigHorizontal2D);
+                $entity.$add('ngMarkIfOutsideOfTheViewPortHorizontal2D',
+                    outsideConfigHorizontal2D);
             }
             if ($entity.ngCyclic.patternHeight) {
-                insideConfigVertical.minHeight = $entity.ngCyclic.patternHeight;
-                $entity.$add('ngMarkIfInsideOfTheViewPortVertical2D', insideConfigVertical);
+                insideConfigVertical2D.minHeight = $entity.ngCyclic.patternHeight;
+                $entity.$add('ngMarkIfInsideOfTheViewPortVertical2D',
+                    insideConfigVertical2D);
                 outsideConfigVertical2D.minHeight = $entity.ngCyclic.patternHeight;
-                $entity.$add('ngMarkIfOutsideOfTheViewPortVertical2D', outsideConfigVertical2D);
+                $entity.$add('ngMarkIfOutsideOfTheViewPortVertical2D',
+                    outsideConfigVertical2D);
             }
         }]
     });
 
+    /**
+     * Define handler for entity that goes outside
+     *
+     * @private
+     * @ignore
+     * @type {{handler: Function}}
+     */
     var outsideConfigHorizontal2D = {
         handler: function($entity, lowerEdge, higherEdge) {
             if (lowerEdge) {
+                //if goes left place to right side
                 $entity.ng2D.x += $entity.ngCyclic.patternWidth;
             } else if (higherEdge) {
+                //if goes right place to left side
                 $entity.ng2D.x -= $entity.ngCyclic.patternWidth;
             }
 
+            //handle of returning to the viewport
             if (!$entity.ngMarkIfInsideOfTheViewPortHorizontal2D) {
-                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal2D', insideConfigHorizontal2D);
+                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal2D',
+                    insideConfigHorizontal2D);
             }
 
+            //stop handling of goes outside
             $entity.$remove('ngMarkIfOutsideOfTheViewPortHorizontal2D');
         }
     };
 
+    /**
+     * Define handler for entity that goes outside
+     *
+     * @private
+     * @ignore
+     * @type {{handler: Function}}
+     */
     var outsideConfigVertical2D = {
         handler: function($entity, lowerEdge, higherEdge) {
             if (lowerEdge) {
@@ -110,32 +188,53 @@
                 $entity.ng2D.y -= $entity.ngCyclic.patternHeight;
             }
 
+            //handle of returning to the viewport
             if (!$entity.ngMarkIfInsideOfTheViewPortVertical2D) {
-                $entity.$add('ngMarkIfInsideOfTheViewPortVertical2D', insideConfigVertical2D);
+                $entity.$add('ngMarkIfInsideOfTheViewPortVertical2D',
+                    insideConfigVertical2D);
             }
 
+            //stop handling of goes outside
             $entity.$remove('ngMarkIfOutsideOfTheViewPortVertical2D');
         }
     };
 
+    /**
+     * Start handle again if goes inside of viewPort
+     *
+     * @private
+     * @ignore
+     * @type {{marker: {ngMarkIfOutsideOfTheViewPortHorizontal2D: {handler: Function}}}}
+     */
     var insideConfigHorizontal2D = {
         marker: {
             ngMarkIfOutsideOfTheViewPortHorizontal2D: outsideConfigHorizontal2D
         }
     };
 
+    /**
+     * Start handle again if goes inside of viewPort
+     *
+     * @private
+     * @ignore
+     * @type {{marker: {ngMarkIfOutsideOfTheViewPortVertical2D: {handler: Function}}}}
+     */
     var insideConfigVertical2D = {
         marker: {
             ngMarkIfOutsideOfTheViewPortVertical2D: outsideConfigVertical2D
         }
     };
 
+    /**
+     * System that waits for entity goes outside of viewport in vertical dimension and handle that situation
+     */
     m.$s('ngMarkIfOutsideOfTheViewPortVertical2D', {
         $require: ['ng2D', 'ngMarkIfOutsideOfTheViewPortVertical2D'],
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfOutsideOfTheViewPortVertical2D;
-            var viewPortHeight = component.viewPortHeight || 0.5 * Math.max(ng2DViewPort.height, component.minHeight);
+            var viewPortHeight = component.viewPortHeight ||
+                0.5 * Math.max(ng2DViewPort.height, component.minHeight);
             component.viewPortHeight = viewPortHeight;
 
             outsideOf($entity,
@@ -149,12 +248,16 @@
         }]
     });
 
+    /**
+     * System that waits for entity goes outside of viewport in horizontal dimension and handle that situation
+     */
     m.$s('ngMarkIfOutsideOfTheViewPortHorizontal2D', {
         $require: ['ng2D', 'ngMarkIfOutsideOfTheViewPortHorizontal2D'],
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfOutsideOfTheViewPortHorizontal2D;
-            var viewPortWidth = component.viewPortWidth || 0.5 * Math.max(ng2DViewPort.width, component.minWidth);
+            var viewPortWidth = component.viewPortWidth ||
+                0.5 * Math.max(ng2DViewPort.width, component.minWidth);
             component.viewPortWidth = viewPortWidth;
 
             outsideOf($entity,
@@ -168,12 +271,16 @@
         }]
     });
 
+    /**
+     * System that waits for entity goes inside of viewport in vertical dimension and handle that situation
+     */
     m.$s('ngMarkIfInsideOfTheViewPortVertical2D', {
         $require: ['ng2D', 'ngMarkIfInsideOfTheViewPortVertical2D'],
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfInsideOfTheViewPortVertical2D;
-            var viewPortHeight = component.viewPortHeight || 0.5 * Math.max(ng2DViewPort.height, component.minHeight);
+            var viewPortHeight = component.viewPortHeight ||
+                0.5 * Math.max(ng2DViewPort.height, component.minHeight);
             component.viewPortHeight = viewPortHeight;
 
             insideOf($entity,
@@ -187,12 +294,16 @@
         }]
     });
 
+    /**
+     * System that waits for entity goes inside of viewport in horizontal dimension and handle that situation
+     */
     m.$s('ngMarkIfInsideOfTheViewPortHorizontal2D', {
         $require: ['ng2D', 'ngMarkIfInsideOfTheViewPortHorizontal2D'],
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfInsideOfTheViewPortHorizontal2D;
-            var viewPortWidth = component.viewPortWidth || 0.5 * Math.max(ng2DViewPort.width, component.minWidth);
+            var viewPortWidth = component.viewPortWidth ||
+                0.5 * Math.max(ng2DViewPort.width, component.minWidth);
             component.viewPortWidth = viewPortWidth;
 
             insideOf($entity,
@@ -214,8 +325,21 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfOutsideOfTheViewPortVertical3D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes outside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
@@ -223,8 +347,21 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfOutsideOfTheViewPortHorizontal3D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes outside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
@@ -232,8 +369,21 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfInsideOfTheViewPortVertical3D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes inside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
@@ -241,26 +391,43 @@
      * Mark entity by component if it outside the ViewPort
      */
     m.$c('ngMarkIfInsideOfTheViewPortHorizontal3D', {
+        /**
+         * handler callback get $entity and top/bottom edge
+         */
         handler: null,
+
+        /**
+         * apply component-markers for any $entity that goes inside
+         * can be {string} name of component, or object with key, value
+         * where key is component name, value is config of component
+         */
         marker: null,
+
+        /**
+         * auto-remove current component
+         */
         autoRemove: true
     });
 
-    m.$s('ngCyclic3DLayer', {
+    m.$s('ng3DCyclicLayer', {
         $require: ['ngCyclic', 'ng3D'],
 
         $addEntity: ['$entity', function($entity) {
             if ($entity.ngCyclic.patternWidth > 0) {
                 insideConfigHorizontal3D.minWidth = $entity.ngCyclic.patternWidth;
-                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal3D', insideConfigHorizontal3D);
+                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal3D',
+                    insideConfigHorizontal3D);
                 outsideConfigHorizontal3D.minWidth = $entity.ngCyclic.patternWidth;
-                $entity.$add('ngMarkIfOutsideOfTheViewPortHorizontal3D', outsideConfigHorizontal3D);
+                $entity.$add('ngMarkIfOutsideOfTheViewPortHorizontal3D',
+                    outsideConfigHorizontal3D);
             }
             if ($entity.ngCyclic.patternHeight > 0) {
                 insideConfigVertical3D.minHeight = $entity.ngCyclic.patternHeight;
-                $entity.$add('ngMarkIfInsideOfTheViewPortVertical3D', insideConfigVertical3D);
+                $entity.$add('ngMarkIfInsideOfTheViewPortVertical3D',
+                    insideConfigVertical3D);
                 outsideConfigVertical3D.minHeight = $entity.ngCyclic.patternHeight;
-                $entity.$add('ngMarkIfOutsideOfTheViewPortVertical3D', outsideConfigVertical3D);
+                $entity.$add('ngMarkIfOutsideOfTheViewPortVertical3D',
+                    outsideConfigVertical3D);
             }
         }]
     });
@@ -274,7 +441,8 @@
             }
 
             if (!$entity.ngMarkIfInsideOfTheViewPortHorizontal3D) {
-                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal3D', insideConfigHorizontal3D);
+                $entity.$add('ngMarkIfInsideOfTheViewPortHorizontal3D',
+                    insideConfigHorizontal3D);
             }
 
             $entity.$remove('ngMarkIfOutsideOfTheViewPortHorizontal3D');
@@ -290,7 +458,8 @@
             }
 
             if (!$entity.ngMarkIfInsideOfTheViewPortVertical3D) {
-                $entity.$add('ngMarkIfInsideOfTheViewPortVertical3D', insideConfigVertical3D);
+                $entity.$add('ngMarkIfInsideOfTheViewPortVertical3D',
+                    insideConfigVertical3D);
             }
 
             $entity.$remove('ngMarkIfOutsideOfTheViewPortVertical3D');
@@ -314,7 +483,8 @@
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfOutsideOfTheViewPortVertical3D;
-            var viewPortHeight = component.viewPortHeight || 0.5 * Math.max(ng2DViewPort.height, component.minHeight);
+            var viewPortHeight = component.viewPortHeight ||
+                0.5 * Math.max(ng2DViewPort.height, component.minHeight);
             component.viewPortHeight = viewPortHeight;
 
             outsideOf($entity,
@@ -333,7 +503,8 @@
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfOutsideOfTheViewPortHorizontal3D;
-            var viewPortWidth = component.viewPortWidth || 0.5 * Math.max(ng2DViewPort.width, component.minWidth);
+            var viewPortWidth = component.viewPortWidth ||
+                0.5 * Math.max(ng2DViewPort.width, component.minWidth);
             component.viewPortWidth = viewPortWidth;
 
             outsideOf($entity,
@@ -352,7 +523,8 @@
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfInsideOfTheViewPortVertical3D;
-            var viewPortHeight = component.viewPortHeight || 0.5 * Math.max(ng2DViewPort.height, component.minHeight);
+            var viewPortHeight = component.viewPortHeight ||
+                0.5 * Math.max(ng2DViewPort.height, component.minHeight);
             component.viewPortHeight = viewPortHeight;
 
             insideOf($entity,
@@ -371,7 +543,8 @@
 
         $update: ['$entity', 'ng2DViewPort', function($entity, ng2DViewPort) {
             var component = $entity.ngMarkIfInsideOfTheViewPortHorizontal3D;
-            var viewPortWidth = component.viewPortWidth || 0.5 * Math.max(ng2DViewPort.width, component.minWidth);
+            var viewPortWidth = component.viewPortWidth ||
+                0.5 * Math.max(ng2DViewPort.width, component.minWidth);
             component.viewPortWidth = viewPortWidth;
 
             insideOf($entity,
@@ -398,7 +571,8 @@
      * @param viewPortPosition
      * @param viewPortSize
      */
-    function outsideOf($entity, componentName, component, componentPosition, viewPortPosition, viewPortSize) {
+    function outsideOf($entity, componentName, component,
+                       componentPosition, viewPortPosition, viewPortSize) {
         var crossBottom = viewPortPosition + viewPortSize < componentPosition,
             crossTop = componentPosition < viewPortPosition - viewPortSize;
 
@@ -426,7 +600,8 @@
      * @param viewPortPosition
      * @param viewPortSize
      */
-    function insideOf($entity, componentName, component, componentPosition, viewPortPosition, viewPortSize) {
+    function insideOf($entity, componentName, component,
+                      componentPosition, viewPortPosition, viewPortSize) {
         var crossBottom = viewPortPosition + viewPortSize >= componentPosition,
             crossTop = componentPosition >= viewPortPosition - viewPortSize;
 
@@ -440,6 +615,16 @@
         }
     }
 
+    /**
+     * Execute handle if has it defined
+     *
+     * @private
+     * @ignore
+     * @param handler
+     * @param $entity
+     * @param lowerEdge
+     * @param higherEdge
+     */
     function callIfHasHandler(handler, $entity, lowerEdge, higherEdge) {
         if (handler) {
             handler($entity, lowerEdge, higherEdge);
