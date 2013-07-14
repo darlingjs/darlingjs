7ff38458bf2a31e484afd024bc297f68d2861afa
Eugene Krevenets <hyzhak@ya.ru>
2013-07-14 14:18:03 +0200

    put some helper function in a box2.js module

--- src/extensions/box2dEmscripten/box2dEmscriptenAdapter.js
+++ src/extensions/box2dEmscripten/box2dEmscriptenAdapter.js
@@ -18,7 +18,12 @@
      *
      */
 
-    //using(Box2D, "b2.+");
+    var e_shapeBit = 0x0001;
+    var e_jointBit = 0x0002;
+    var e_aabbBit = 0x0004;
+    var e_pairBit = 0x0008;
+    var e_centerOfMassBit = 0x0010;
+
 
     m.$s('ngBox2DSystem', {
         gravity: {x:0.0, y:0.0},
@@ -642,11 +647,11 @@
             this._mouseDownHandler = function(e) {
                 self._isMouseDown = true;
                 self._handleMouseMove(e);
-                document.addEventListener("mousemove", this._mouseMoveHandler, true);
+                document.addEventListener("mousemove", self._mouseMoveHandler, true);
             };
 
             this._mouseUpHandler = function() {
-                document.removeEventListener("mousemove", this._mouseMoveHandler, true);
+                document.removeEventListener("mousemove", self._mouseMoveHandler, true);
                 self._isMouseDown = false;
             };
 
@@ -1756,6 +1761,66 @@
         return instance;
     }
 
+    /**
+     * some helper functions
+     */
+    //to replace original C++ operator =
+    function copyVec2(vec) {
+        return new Box2D.b2Vec2(vec.get_x(), vec.get_y());
+    }
+
+    //to replace original C++ operator * (float)
+    function scaleVec2(vec, scale) {
+        vec.set_x( scale * vec.get_x() );
+        vec.set_y( scale * vec.get_y() );            
+    }
+
+    //to replace original C++ operator *= (float)
+    function scaledVec2(vec, scale) {
+        return new Box2D.b2Vec2(scale * vec.get_x(), scale * vec.get_y());
+    }
 
 
+    // http://stackoverflow.com/questions/12792486/emscripten-bindings-how-to-create-an-accessible-c-c-array-from-javascript
+    function createChainShape(vertices, closedLoop) {
+        var shape = new Box2D.b2ChainShape();
+        var buffer = Box2D.allocate(vertices.length * 8, 'float', Box2D.ALLOC_STACK);
+        var offset = 0;
+        for (var i=0;i<vertices.length;i++) {
+            Box2D.setValue(buffer+(offset), vertices[i].get_x(), 'float'); // x
+            Box2D.setValue(buffer+(offset+4), vertices[i].get_y(), 'float'); // y
+            offset += 8;
+        }            
+        var ptr_wrapped = Box2D.wrapPointer(buffer, Box2D.b2Vec2);
+        if ( closedLoop )
+            shape.CreateLoop(ptr_wrapped, vertices.length);
+        else
+            shape.CreateChain(ptr_wrapped, vertices.length);
+        return shape;
+    }
+
+    function createPolygonShape(vertices) {
+        var shape = new Box2D.b2PolygonShape();
+        var buffer = Box2D.allocate(vertices.length * 8, 'float', Box2D.ALLOC_STACK);
+        var offset = 0;
+        for (var i=0;i<vertices.length;i++) {
+            Box2D.setValue(buffer+(offset), vertices[i].get_x(), 'float'); // x
+            Box2D.setValue(buffer+(offset+4), vertices[i].get_y(), 'float'); // y
+            offset += 8;
+        }            
+        var ptr_wrapped = Box2D.wrapPointer(buffer, Box2D.b2Vec2);
+        shape.Set(ptr_wrapped, vertices.length);
+        return shape;
+    }
+
+    function createRandomPolygonShape(radius) {
+        var numVerts = 3.5 + Math.random() * 5;
+        numVerts = numVerts | 0;
+        var verts = [];
+        for (var i = 0; i < numVerts; i++) {
+            var angle = i / numVerts * 360.0 * 0.0174532925199432957;
+            verts.push( new Box2D.b2Vec2( radius * Math.sin(angle), radius * -Math.cos(angle) ) );
+        }            
+        return createPolygonShape(verts);
+    }
 })(darlingjs, darlingutil);
