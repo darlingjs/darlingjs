6b837e8b406c3c97da8d462c142326cfc6649716
Eugene Krevenets <hyzhak@ya.ru>
2015-07-26 00:04:34 +0200

    #25 mixin methods star/stop of runner to pipeline

--- test/pipelineSpec.js
+++ test/pipelineSpec.js
@@ -1,10 +1,15 @@
 'use strict';
 
+var chai = require('chai');
 var expect = require('chai').expect;
 var faker = require('faker');
 var Pipeline = require('../lib/core/pipeline');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
 var testWorldInstance = require('./worldInstanceSpecHelper');
 var darling = require('../');
+
+chai.use(sinonChai);
 
 describe('pipeline', function() {
   var pipeline;
@@ -28,6 +33,25 @@ describe('pipeline', function() {
         step = _step_;
       });
       expect(step).to.be.a('function');
+    });
+
+    it('should mixin start/stop methods of runner to pipeline', function() {
+      var start = sinon.spy(),
+        stop = sinon.spy();
+
+      pipeline = pipeline.live(function(step) {
+        return {
+          start: start,
+          stop: stop
+        };
+      });
+
+      expect(pipeline).to.have.property('start');
+      expect(pipeline).to.have.property('stop');
+      pipeline.start();
+      expect(start).to.have.been.calledOnce;
+      pipeline.stop();
+      expect(stop).to.have.been.calledOnce;
     });
   });
 
--- test/utils/wrapSpec.js
+++ test/utils/wrapSpec.js
@@ -3,11 +3,11 @@ var wrap = require('../../utils').wrap;
 
 describe('util', function() {
   describe('wrap', function() {
-    var wrapped;
     var wrapper;
+    var wrapped;
 
     beforeEach(function() {
-      wrapped = {
+      wrapper = {
         f1: function() {
           return this;
         },
@@ -17,7 +17,7 @@ describe('util', function() {
         }
       };
 
-      wrapper = {
+      wrapped = {
         f3: function() {
           return this;
         }
@@ -26,50 +26,50 @@ describe('util', function() {
 
     it('should forward methods of target to wrapper', function() {
       wrap(wrapped, wrapper);
-      expect(wrapper).to.have.property('f1');
-      expect(wrapper.f1).to.be.a('function');
-      expect(wrapper).to.have.property('f2');
-      expect(wrapper.f2).to.be.a('function');
+      expect(wrapped).to.have.property('f1');
+      expect(wrapped.f1).to.be.a('function');
+      expect(wrapped).to.have.property('f2');
+      expect(wrapped.f2).to.be.a('function');
     });
 
     it('should keep wrapper original methods', function() {
       var orgFunc = function() {};
-      wrapper.f2 = orgFunc;
+      wrapped.f2 = orgFunc;
       wrap(wrapped, wrapper);
-      expect(wrapper.f2).to.be.equal(orgFunc);
+      expect(wrapped.f2).to.be.equal(orgFunc);
     });
 
     it('should keep this of function equal to target', function() {
       wrap(wrapped, wrapper);
 
-      expect(wrapper.f1()).to.be.equal(wrapped);
-      expect(wrapper.f2()).to.be.equal(wrapped);
-      expect(wrapper.f3()).to.be.equal(wrapper);
+      expect(wrapped.f1()).to.be.equal(wrapper);
+      expect(wrapped.f2()).to.be.equal(wrapper);
+      expect(wrapped.f3()).to.be.equal(wrapped);
     });
 
     it('should pass by undefined or null target', function() {
-      wrap(null, wrapper);
-      expect(wrapper).to.not.have.property('f1');
-      expect(wrapper).to.not.have.property('f2');
-      expect(wrapper).to.have.property('f3');
+      wrap(wrapped, null);
+      expect(wrapped).to.not.have.property('f1');
+      expect(wrapped).to.not.have.property('f2');
+      expect(wrapped).to.have.property('f3');
     });
 
     it('should return wrapper', function() {
-      expect(wrap(wrapped, wrapper)).to.equal(wrapper);
+      expect(wrap(wrapped, wrapper)).to.equal(wrapped);
     });
 
     it('should wrap instance of classes as well', function() {
       var SomeClass = function() {};
       SomeClass.prototype.f1 = function() {};
       SomeClass.prototype.f2 = function() {};
-      wrapped = new SomeClass();
+      wrapper = new SomeClass();
 
       wrap(wrapped, wrapper);
 
-      expect(wrapper).to.have.property('f1');
-      expect(wrapper.f1).to.be.a('function');
-      expect(wrapper).to.have.property('f2');
-      expect(wrapper.f2).to.be.a('function');
+      expect(wrapped).to.have.property('f1');
+      expect(wrapped.f1).to.be.a('function');
+      expect(wrapped).to.have.property('f2');
+      expect(wrapped.f2).to.be.a('function');
     });
   });
 });
